ff93c1485faf479bb0e9bcaf39ae430e11e02a3f
Bradley Saul <bsaul@novisci.com>
2022-08-22 10:05:22 -0400

    add show instance

--- silly/src/MyLib.hs
+++ silly/src/MyLib.hs
@@ -2,9 +2,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE StandaloneDeriving #-}
 module MyLib where
 
 import Dhall.TH
 
 Dhall.TH.makeHaskellTypes
   [ SingleConstructor "MyType" "MkMyType" "./src/MyType.dhall"]
+
+deriving instance Show MyType
